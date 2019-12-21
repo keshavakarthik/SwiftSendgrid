a721bc4cd2a44a91dbe8211c3460b5d817456c9a
Keshava Karthik S <kesh1988@gmail.com>
2019-12-21 23:03:04 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-sendgrid
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_sendgridTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_sendgridTests.allTests()
+XCTMain(tests)
