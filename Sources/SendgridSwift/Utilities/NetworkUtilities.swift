c4637fd8136cf150f9f30ee4aec825b922af72ff
Keshava Karthik S <kesh1988@gmail.com>
2019-12-25 13:10:43 +0530

    Network Interfaces Added

--- Sources/SendgridSwift/Utilities/NetworkUtilities.swift
+++ Sources/SendgridSwift/Utilities/NetworkUtilities.swift
@@ -8,38 +8,112 @@
 import Foundation
 import PerfectCURL
 
-public final class NetworkUtility {
+
+public final class NetworkUtilities {
     
-    public static let shared = NetworkUtility()
+    typealias HeaderNames = [(PerfectCURL.CURLRequest.Header.Name, String)]
+    
+    public static let shared = NetworkUtilities()
     private init () {
     }
     
-    public func asynchronousMailSend(_ targetURL: String, authorization:String, jsonString: String,
-                                       completion: @escaping(Bool,[String:Any])->Void) {
+    /// Synchronous Requests
+    /// Return Response Code , Reponse String and Reponse JSON
+    func synchronousNetworkRequest(_ requestUrl:String, httpMethod: HTTPMethod, authorization: String,
+                                          postString: String, headers:HeaderNames) -> (Int?,String?,[String:Any]?) {
         
+       
         
-        let curlRequest = CURLRequest(targetURL,
-                                      .httpMethod(.post),
-                                      .postString(jsonString),
-                                      .addHeader(.contentType,"application/json"),
-                                      .addHeader(.authorization, authorization))
-        curlRequest.perform() {
-            confirmation in
-            do {
-                let sendgridResponse = try confirmation()
-                let statusCode = sendgridResponse.responseCode
-                if statusCode >= 200 && statusCode <= 300 {
-                    completion (true, ["status":"SUCCESS","message":"REQUEST SUCCEEDED. MAIL SENT SUCCESSFULLY"])
-                    return
-                } else {
-                    let errorMessage = sendgridResponse.bodyString
-                    completion (false, ["status":"FAILURE","message":"REQUEST FAILED WITH ERROR MESSAGE : \(errorMessage)"])
-                    return
-                }
+        switch httpMethod {
+         case .get,
+              .head,
+              .options:
+             do {
+                let curlResponse = try CURLRequest(requestUrl,.httpMethod(httpMethod),
+                                            .addHeaders(headers),.addHeader(.authorization,
+                                            authorization)).perform()
+            
+                let response        = curlResponse
+                let responseCode    = response.responseCode
+                let responseString  = response.bodyString
+                let responseJSON    = response.bodyJSON
+                return (responseCode,responseString,responseJSON)
+             } catch {
+                return (400,"Request Failed",nil)
+             }
+            
+         case .post,
+              .put,
+              .delete :
+             do {
+                let curlResponse = try CURLRequest(requestUrl,.httpMethod(httpMethod),
+                                           .postString(postString),
+                                           .addHeaders(headers),.addHeader(.authorization,
+                                           authorization)).perform()
+           
+               let response        = curlResponse
+               let responseCode    = response.responseCode
+               let responseString  = response.bodyString
+               let responseJSON    = response.bodyJSON
+               return (responseCode,responseString,responseJSON)
             } catch {
-                completion (false, ["status":"FAILURE","message":"REQUEST FAILED"])
-                return
+               return (400,"Request Failed",nil)
             }
+        default:
+            return (400,nil,nil)
         }
     }
+    
+    func asynchronousNetworkRequest(_ requestUrl:String, httpMethod: HTTPMethod, authorization: String,
+                                             postString: String, headers:HeaderNames,
+                                             completion:@escaping(Int?,String?,[String:Any]?)->Void) {
+           switch httpMethod {
+            case .get,
+                 .head,
+                 .options:
+                
+                    CURLRequest(requestUrl,.httpMethod(httpMethod),
+                                .addHeaders(headers),
+                                .addHeader(.authorization,authorization))
+                        .perform
+                        {
+                            confirmation in
+                                do {
+                                   let response        = try confirmation()
+                                   let responseCode    = response.responseCode
+                                   let responseString  = response.bodyString
+                                   let responseJSON    = response.bodyJSON
+                                   completion (responseCode,responseString,responseJSON)
+                                   return
+                                } catch {
+                                   completion (400,"Request Failed",nil)
+                                   return
+                                }
+                            
+                        }
+            case .post,
+                 .put,
+                 .delete :
+                CURLRequest(requestUrl,.httpMethod(httpMethod),
+                            .postString(postString),.addHeaders(headers),
+                            .addHeader(.authorization,authorization))
+                    .perform {
+                        confirmation in
+                            do {
+                               let response        = try confirmation()
+                               let responseCode    = response.responseCode
+                               let responseString  = response.bodyString
+                               let responseJSON    = response.bodyJSON
+                               completion (responseCode,responseString,responseJSON)
+                               return
+                            } catch {
+                               completion (400,"Request Failed",nil)
+                               return
+                            }
+               }
+           default:
+               completion (400,nil,nil)
+               return
+           }
+       }
 }
